65cf006485709a7bf634c4fb3fbce712e6663703
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-03-21 01:48:49 -0600

    Introduction to JSX is now complete in the index.js and in the notes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,99 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+////////////////////////////////////////////////////////////
+//EMBEDDING EXPRESSIONS IN JSX(SUCCESS)
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  <h1>Hello, Class!</h1>,
   document.getElementById('root')
 )
+
+function people(person, message){
+  return person + ' was always ' + message;
+}
+
+const name = (
+  <h1>
+    {people('Teako', 'the smartest')}<br/>
+    {people('Jay', 'super loco')}<br/>
+    {people('Matt', 'a genie out of a bottle')}<br/>
+    {people('Brandon', 'kinda intense')}
+  </h1>
+)
+
+ReactDOM.render(
+  name,
+  document.getElementById('people'),
+)
+
+
+/////////////////////////////////////////////////////////////
+// ADDING JSX TO REGULAR JS (UNSUCCESFUL)
+// if(name = null){
+//   return 'You need to put in some values!'
+// } else {
+//    return name
+// }
+
+
+/////////////////////////////////////////////////////////////
+//SPECIFYING ATTRIBUTES WITH JSX: I still don't fully know how this works, make sure you ask Teako
+//(SUCCESFUL!)
+
+//You may use quites to specify string literals as attributes
+// const element = <div tabIndex="0"></div>
+
+//You may also use curly braces to embed a JS expression in an attribute.
+// const secondElement = <img src={user.avatarUrl}></img>
+
+function classShifter(name){
+  return name
+}
+
+const bodySection = (
+  <div className={classShifter('red')}>"Try this section"</div>
+)
+
+const bodyTwo = (
+  <div className={classShifter('blue')}>'This section is supposed to be blue!'</div>
+)
+
+
+ReactDOM.render(
+  bodySection,
+  document.getElementById('colors')
+)
+
+ReactDOM.render(
+    bodyTwo,
+    document.getElementById('colors2')
+)
+
+//SPECIFYING CHILDREN WITH JSX
+//NOTICE: if a tag is empty, you may close it immediately with />, like XML
+//(SUCCESSFUL)
+  let topDescription = (
+    <div>
+      <section className= 'myTexts'>
+        <h3>'This is just one sample text'</h3>
+        <h3>'This is another sample text'</h3>
+      </section>
+    </div>
+  )
+
+  ReactDOM.render(
+    topDescription,
+    document.getElementById('description1')
+  )
+
+// CONVERTING IT TO CREATEELEMENT
+//(UNSUCCESSFUL: I don't know how to embed children elements using this format)
+
+  // let topDescription = React.createElement(
+  //   'div',
+  //   'section',
+  //   {className: 'myTexts'},
+  //
+  // )
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
